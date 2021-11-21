c10f08253b49b34df34199428531cf653747e896
guangcai1011 <1763452998@qq.com>
2021-11-21 17:51:12 +0800

    add elementUI

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/about",
@@ -14,24 +14,24 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
     path: "/test",
     name: "Test",
-    component: () => import(/* webpackChunkName: "test" */ "../views/Test.vue"),
+    component: () => import(/* webpackChunkName: "test" */ "../views/Test.vue")
   },
   {
     path: "/test1",
     name: "Test1",
     component: () =>
-      import(/* webpackChunkName: "test1" */ "../views/Test1.vue"),
-  },
+      import(/* webpackChunkName: "test1" */ "../views/Test1.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
